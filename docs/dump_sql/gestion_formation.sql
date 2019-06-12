9604ef6e57e7999728303002d4450d4f1371417d
Sulpice Remi <remitekky@hotmail.fr>
2019-06-12 08:09:19 +0200

    New db version

--- docs/dump_sql/gestion_formation.sql
+++ docs/dump_sql/gestion_formation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 09 juin 2019 à 14:56
+-- Généré le :  mer. 12 juin 2019 à 06:08
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -44,6 +44,31 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `bar`
+--
+
+DROP TABLE IF EXISTS `bar`;
+CREATE TABLE IF NOT EXISTS `bar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `f1` int(11) NOT NULL,
+  `f2` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `f3` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `bar`
+--
+
+INSERT INTO `bar` (`id`, `f1`, `f2`, `f3`) VALUES
+(1, 95, 'aaaaaa', 'zzzzzz'),
+(2, 95, 'qqqq', 'ssss'),
+(3, 65, 'wwww', 'xxxx'),
+(4, 325, 'oooo', 'pppp');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `candidater`
 --
 
@@ -62,7 +87,138 @@ CREATE TABLE IF NOT EXISTS `candidater` (
 --
 
 INSERT INTO `candidater` (`session_id`, `participant_id`, `accepter`, `motif_refus`) VALUES
-(30, 3, 1, NULL);
+(5, 47, 1, NULL),
+(27, 13, 1, NULL),
+(23, 18, 0, NULL),
+(24, 5, 0, NULL),
+(6, 28, 1, NULL),
+(19, 12, 1, NULL),
+(9, 20, 0, NULL),
+(23, 28, 1, NULL),
+(30, 11, 1, NULL),
+(30, 2, 0, NULL),
+(8, 10, 0, NULL),
+(30, 33, 1, NULL),
+(18, 8, 1, NULL),
+(1, 26, 1, NULL),
+(7, 45, 0, NULL),
+(23, 38, 0, NULL),
+(9, 13, 1, NULL),
+(10, 10, 1, NULL),
+(3, 8, 0, NULL),
+(20, 40, 1, NULL),
+(22, 21, 1, NULL),
+(30, 49, 1, NULL),
+(15, 24, 1, NULL),
+(8, 23, 1, NULL),
+(29, 37, 1, NULL),
+(21, 3, 1, NULL),
+(24, 32, 0, NULL),
+(2, 24, 0, NULL),
+(28, 50, 1, NULL),
+(14, 30, 1, NULL),
+(21, 6, 1, NULL),
+(21, 49, 0, NULL),
+(3, 41, 0, NULL),
+(9, 19, 0, NULL),
+(13, 20, 1, NULL),
+(24, 14, 0, NULL),
+(26, 13, 0, NULL),
+(6, 48, 0, NULL),
+(7, 35, 1, NULL),
+(10, 24, 1, NULL),
+(22, 1, 0, NULL),
+(4, 17, 1, NULL),
+(26, 16, 1, NULL),
+(7, 44, 0, NULL),
+(2, 22, 1, NULL),
+(27, 1, 1, NULL),
+(17, 19, 0, NULL),
+(19, 34, 1, NULL),
+(6, 13, 1, NULL),
+(2, 38, 1, NULL),
+(7, 23, 0, NULL),
+(21, 47, 1, NULL),
+(25, 17, 0, NULL),
+(16, 24, 0, NULL),
+(22, 28, 0, NULL),
+(11, 12, 1, NULL),
+(30, 2, 0, NULL),
+(30, 45, 1, NULL),
+(15, 28, 0, NULL),
+(28, 17, 0, NULL),
+(1, 2, 0, NULL),
+(14, 33, 0, NULL),
+(15, 11, 0, NULL),
+(9, 33, 0, NULL),
+(7, 47, 0, NULL),
+(26, 15, 1, NULL),
+(28, 11, 1, NULL),
+(28, 6, 0, NULL),
+(8, 19, 0, NULL),
+(19, 6, 0, NULL),
+(22, 30, 1, NULL),
+(16, 26, 1, NULL),
+(27, 45, 1, NULL),
+(7, 9, 0, NULL),
+(28, 38, 0, NULL),
+(4, 32, 0, NULL),
+(18, 34, 1, NULL),
+(1, 49, 0, NULL),
+(6, 3, 0, NULL),
+(12, 36, 0, NULL),
+(6, 29, 1, NULL),
+(9, 14, 0, NULL),
+(24, 36, 1, NULL),
+(6, 37, 1, NULL),
+(16, 48, 0, NULL),
+(1, 11, 0, NULL),
+(28, 8, 0, NULL),
+(27, 18, 0, NULL),
+(11, 34, 0, NULL),
+(18, 5, 0, NULL),
+(19, 42, 1, NULL),
+(14, 34, 1, NULL),
+(6, 42, 0, NULL),
+(11, 19, 1, NULL),
+(25, 39, 1, NULL),
+(23, 9, 1, NULL),
+(12, 32, 1, NULL),
+(16, 48, 0, NULL),
+(9, 18, 1, NULL),
+(2, 45, 0, NULL),
+(7, 1, 1, NULL),
+(11, 1, 0, 'Pas disponible'),
+(13, 1, 0, 'Pas disponible'),
+(22, 1, 1, NULL),
+(23, 1, 0, 'Pas disponible'),
+(30, 3, 1, NULL),
+(4, 1, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `foo`
+--
+
+DROP TABLE IF EXISTS `foo`;
+CREATE TABLE IF NOT EXISTS `foo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `f1` int(11) NOT NULL,
+  `f2` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `f3` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `foo`
+--
+
+INSERT INTO `foo` (`id`, `f1`, `f2`, `f3`) VALUES
+(1, 3216546, 'aaaaaaa', 'aaaaaaaa'),
+(2, 438654, 'bbbbbb', 'bbbbbb'),
+(3, 65, 'cccc', 'ccc'),
+(4, 84, 'ddddddd', '');
 
 -- --------------------------------------------------------
 
@@ -75,7 +231,7 @@ CREATE TABLE IF NOT EXISTS `formation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `formation`
@@ -90,7 +246,8 @@ INSERT INTO `formation` (`id`, `name`) VALUES
 (7, 'Shoes'),
 (8, 'Clothing'),
 (9, 'Clothing'),
-(10, 'Grocery');
+(10, 'Grocery'),
+(11, 'Test');
 
 -- --------------------------------------------------------
 
@@ -186,7 +343,14 @@ INSERT INTO `interesser` (`formation_id`, `participant_id`) VALUES
 (10, 24),
 (10, 10),
 (2, 64),
-(3, 64);
+(3, 64),
+(2, 1),
+(6, 1),
+(7, 1),
+(5, 1),
+(2, 65),
+(3, 65),
+(4, 65);
 
 -- --------------------------------------------------------
 
@@ -202,7 +366,7 @@ CREATE TABLE IF NOT EXISTS `participant` (
   `email` varchar(255) NOT NULL,
   `telephone` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `participant`
@@ -259,7 +423,8 @@ INSERT INTO `participant` (`id`, `name`, `first_name`, `email`, `telephone`) VAL
 (48, 'Moodey', 'Gwenette', 'gmoodey1b@ocn.ne.jp', '2147483647'),
 (49, 'O\'Lynn', 'Andrew', 'aolynn1c@msn.com', '2147483647'),
 (50, 'Sprigin', 'Kippy', 'ksprigin1d@indiegogo.com', '1727247636'),
-(64, 'Sulpice', 'Réli', 'cqsd', '3216549870');
+(64, 'Sulpice', 'Réli', 'cqsd', '3216549870'),
+(65, 'dydyddg', 'dgdfg', 'dfxv', '94');
 
 -- --------------------------------------------------------
 
@@ -277,7 +442,7 @@ CREATE TABLE IF NOT EXISTS `session` (
   `id_formation` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_formation` (`id_formation`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `session`
@@ -313,7 +478,8 @@ INSERT INTO `session` (`id`, `date`, `hour_begin`, `hour_end`, `place`, `id_form
 (27, '2019-05-12', '10', '15', '9 Jenna Hill', 8),
 (28, '2019-03-21', '9', '17', '023 Lukken Court', 3),
 (29, '2018-07-17', '11', '15', '121 Haas Point', 5),
-(30, '2018-05-21', '10', '13', '63934 Meadow Ridge Avenue', 4);
+(30, '2018-05-21', '10', '13', '63934 Meadow Ridge Avenue', 4),
+(33, '2019-06-10', '0', '8', 'Test', 11);
 
 -- --------------------------------------------------------
 
@@ -341,7 +507,7 @@ INSERT INTO `user` (`id`, `login`, `pass`, `role`, `heurePremiereCo`, `tentative
 (1, 'admin', '88329B8692B8A8A26E0A9A37421B3BD644024CFBF20F928E37510B43B7684B07', 'administrateur', NULL, 0, 0),
 (2, 'gestio', '164A48B61D83B559A94E28357D36CFD7497B3FEB0F6FE2565A29EC5178C8CDDC', 'gestionnaire', NULL, 0, 0),
 (3, 'guest', 'F9529F8231BABB3DF48E2B4F02CF67CA37763761116ED1A7F79EB79DD61A2747', 'utilisateur', NULL, 0, 0),
-(5, 'dev', '5994471ABB01112AFCC18159F6CC74B4F511B99806DA59B3CAF5A9C173CACFC5', 'administrateur', NULL, 0, 1);
+(5, 'dev', 'D505ABEF7DAE3955B45DBED9ECE1C4F498D8C3E1FD3A62F9842744E44D200499', 'administrateur', NULL, 0, 0);
 
 --
 -- Contraintes pour les tables déchargées
